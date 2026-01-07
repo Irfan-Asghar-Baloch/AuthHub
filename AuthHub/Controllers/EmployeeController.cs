23e346f5be1cf7b8a8e25a4d48ddde8ff3de7fad
irfan.asghar <irfankkhan255@gmail.com>
2026-01-07 17:13:15 +0500

    add-only-controller

---
+++ AuthHub/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthHub.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("Test")]
+        public IActionResult Test()
+        {
+            return Ok("testing done");
+        }
+        [Authorize(Roles ="Admin")]
+        [HttpGet("AdminOnly")]
+        public IActionResult AdminOnly()
+        {
+            return Ok("Admin access granted");
+        }
+    }
+}
---
+++ AuthHub/Controllers/RoleController.cs
@@ -0,0 +1,28 @@
+﻿using AuthHub.Application.DTOs;
+using AuthHub.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService; 
+        }
+
+        [HttpPost("AddRole")]
+        public async Task<IActionResult> AddRole(RoleDto role)
+        {
+            var result  = await _roleService.AddAsync(role);    
+            return Ok(result);
+
+        }
+
+
+    }
+}
